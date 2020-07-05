811ef7981c1bf4eedcd3edbd93f02f4597dfb0df
kpoppel <4174578+kpoppel@users.noreply.github.com>
2020-07-05 14:52:56 +0000

    Move user data files.

--- sandbox/kpoppel/stage-1/generate.sh
+++ sandbox/kpoppel/stage-1/generate.sh
@@ -1,20 +1,20 @@
 #!/bin/bash
-if [ -f config_data ]; then
+if [ -f ../config_data ]; then
   echo "Reusing config_data"
-  source config_data
+  source ../config_data
 else
   read -p "Input your domain: " domain
   read -p "Email for use with LetsEncrypt: " acme_email
   read -p "Input user for basic authentication:" user
   read -s -p "Input password for basic authentication:" pass
 
-  echo "domain=$domain" > config_data
-  echo "acme_email=$acme_email" >> config_data
-  echo "user=$user" >> config_data
-  echo "pass=$pass" >> config_data
+  echo "domain=$domain" > ../config_data
+  echo "acme_email=$acme_email" >> ../config_data
+  echo "user=$user" >> ../config_data
+  echo "pass=$pass" >> ../config_data
 
-  chmod 600 config_data
-  chmod 600 basic_auth_credentials
+  chmod 600 ../config_data
+  chmod 600 ../basic_auth_credentials
 fi
 
 sed 's/{{DOMAIN}}/'${domain}'/g' 11-whoami.ingressroute.yaml.j2 > 11-whoami.ingressroute.yaml
@@ -22,8 +22,8 @@ sed 's/{{ACME_EMAIL}}/'${acme_email}'/g' 03-traefik.deployment.yaml.j2 > 03-trae
 
 
 read -n1 -p "Press a key to deploy"
-htpasswd -b -c ./basic_auth_credentials $user $pass
-kubectl create secret generic traefik-admin --from-file ./basic_auth_credentials -n kube-system
+htpasswd -b -c ../basic_auth_credentials $user $pass
+kubectl create secret generic traefik-admin --from-file ../basic_auth_credentials -n kube-system
 
 kubectl apply \
     -f 00-klipperlb.daemonset.yaml  \
